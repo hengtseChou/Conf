d3d8c0cbc02341782e8851157a4baf8af982682c
hengtseChou <hankthedev@gmail.com>
2024-10-02 14:55:18 +0800

    add: a script to symlink to corresponding location

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source ./symlink.sh
+
+symlink ./chromium/chromium-flags.conf --to-config
+symlink ./code/code-flags.conf --to-config
+symlink ./git/.gitconfig --to-home
+symlink ./go/env --custom-dir ~/.config/go
+symlink ./htop --to-config
+symlink ./nano/.nanorc --to-home
+symlink ./npm/.npmrc --to-home
+symlink ./paru --to-config
+symlink ./R/.Rprofile --to-home
+symlink ./R/.Renviron --to-home
+symlink ./spotify/spotify-flags.conf --to-config
