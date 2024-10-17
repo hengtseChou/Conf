84c717721e500a1a764182362c6ff076ae053808
hengtseChou <hankthedev@gmail.com>
2024-10-17 16:14:01 +0800

    add: check if the program exists before apply config

--- setup.sh
+++ setup.sh
@@ -1,17 +1,45 @@
 #!/bin/bash
 source ./symlink.sh
 
-symlink $PWD/chromium/chromium-flags.conf --to-config
-symlink $PWD/code/code-flags.conf --to-config
-symlink $PWD/fcitx5 --to-config
-symlink $PWD/git/.gitconfig --to-home
-symlink $PWD/go/env --custom-dir ~/.config/go
-symlink $PWD/htop --to-config
-sudo cp $PWD/makepkg/makepkg.conf /etc/makepkg.conf
-symlink $PWD/nano/.nanorc --to-home
-symlink $PWD/npm/.npmrc --to-home
-sudo cp $PWD/pacman/pacman.conf /etc/pacman.conf
-symlink $PWD/paru --to-config
-symlink $PWD/R/.Rprofile --to-home
-symlink $PWD/R/.Renviron --to-home
-symlink $PWD/spotify/spotify-flags.conf --to-config
+if command -v chromium 2>&1 >/dev/null; then
+    symlink $PWD/chromium/chromium-flags.conf --to-config
+fi
+if command -v code 2>&1 >/dev/null; then
+    symlink $PWD/code/code-flags.conf --to-config
+fi
+if command -v fcitx5 2>&1 >/dev/null; then
+    symlink $PWD/fcitx5 --to-config
+fi
+if command -v git 2>&1 >/dev/null; then
+    symlink $PWD/git/.gitconfig --to-home
+fi
+if command -v go 2>&1 >/dev/null; then
+    symlink $PWD/go/env --custom-dir ~/.config/go
+fi
+if command -v htop 2>&1 >/dev/null; then
+    symlink $PWD/htop --to-config
+fi
+if command -v makepkg 2>&1 >/dev/null; then
+    echo ":: Copying makepkg.conf to /etc/makepkg.conf"
+    sudo cp $PWD/makepkg/makepkg.conf /etc/makepkg.conf
+fi
+if command -v nano 2>&1 >/dev/null; then
+    symlink $PWD/nano/.nanorc --to-home
+fi
+if command -v npm 2>&1 >/dev/null; then
+    symlink $PWD/npm/.npmrc --to-home
+fi
+if command -v pacman 2>&1 >/dev/null; then
+    echo ":: Copying pacman.conf to /etc/pacman.conf"
+    sudo cp $PWD/pacman/pacman.conf /etc/pacman.conf
+fi
+if command -v paru 2>&1 >/dev/null; then
+    symlink $PWD/paru --to-config
+fi
+if command -v r 2>&1 >/dev/null; then
+    symlink $PWD/R/.Rprofile --to-home
+    symlink $PWD/R/.Renviron --to-home
+fi
+if command -v spotify 2>&1 >/dev/null; then
+    symlink $PWD/spotify/spotify-flags.conf --to-config
+fi
