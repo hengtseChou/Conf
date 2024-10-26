ddd31bd796e89e5382cb362adf42f5f3166a3c0e
hengtseChou <hankthedev@gmail.com>
2024-10-26 21:58:47 +0800

    misc: formattion

--- setup.sh
+++ setup.sh
@@ -2,49 +2,49 @@
 source ./symlink.sh
 
 if command -v chromium 2>&1 >/dev/null; then
-    symlink $PWD/chromium/chromium-flags.conf --to-config
+	symlink $PWD/chromium/chromium-flags.conf --to-config
 fi
 if command -v code 2>&1 >/dev/null; then
-    symlink $PWD/code/code-flags.conf --to-config
+	symlink $PWD/code/code-flags.conf --to-config
 fi
 if command -v fcitx5 2>&1 >/dev/null; then
-    symlink $PWD/fcitx5 --to-config
+	symlink $PWD/fcitx5 --to-config
 fi
 if command -v git 2>&1 >/dev/null; then
-    symlink $PWD/git/.gitconfig --to-home
+	symlink $PWD/git/.gitconfig --to-home
 fi
 if command -v go 2>&1 >/dev/null; then
-    symlink $PWD/go/env --custom-dir ~/.config/go
+	symlink $PWD/go/env --custom-dir ~/.config/go
 fi
 if command -v htop 2>&1 >/dev/null; then
-    symlink $PWD/htop --to-config
+	symlink $PWD/htop --to-config
 fi
 if command -v makepkg 2>&1 >/dev/null; then
-    echo ":: Copying makepkg.conf to /etc/makepkg.conf"
-    sudo cp $PWD/makepkg/makepkg.conf /etc/makepkg.conf
+	echo ":: Copying makepkg.conf to /etc/makepkg.conf"
+	sudo cp $PWD/makepkg/makepkg.conf /etc/makepkg.conf
 fi
 if command -v nano 2>&1 >/dev/null; then
-    symlink $PWD/nano/.nanorc --to-home
+	symlink $PWD/nano/.nanorc --to-home
 fi
 if command -v npm 2>&1 >/dev/null; then
-    symlink $PWD/npm/.npmrc --to-home
+	symlink $PWD/npm/.npmrc --to-home
 fi
 if command -v pacman 2>&1 >/dev/null; then
-    echo ":: Copying pacman.conf to /etc/pacman.conf"
-    sudo cp $PWD/pacman/pacman.conf /etc/pacman.conf
+	echo ":: Copying pacman.conf to /etc/pacman.conf"
+	sudo cp $PWD/pacman/pacman.conf /etc/pacman.conf
 fi
 if command -v paru 2>&1 >/dev/null; then
-    symlink $PWD/paru --to-config
+	symlink $PWD/paru --to-config
 fi
 if command -v r 2>&1 >/dev/null; then
-    symlink $PWD/R/.Rprofile --to-home
-    symlink $PWD/R/.Renviron --to-home
+	symlink $PWD/R/.Rprofile --to-home
+	symlink $PWD/R/.Renviron --to-home
 fi
 if command -v rstudio 2>&1 >/dev/null; then
-    symlink $PWD/rstudio/config.json --custom-dir ~/.config/rstudio
-    symlink $PWD/rstudio/keybindings --custom-dir ~/.config/rstudio
-    symlink $PWD/rstudio/rstudio-prefs.json --custom-dir ~/.config/rstudio
+	symlink $PWD/rstudio/config.json --custom-dir ~/.config/rstudio
+	symlink $PWD/rstudio/keybindings --custom-dir ~/.config/rstudio
+	symlink $PWD/rstudio/rstudio-prefs.json --custom-dir ~/.config/rstudio
 fi
 if command -v spotify 2>&1 >/dev/null; then
-    symlink $PWD/spotify/spotify-flags.conf --to-config
+	symlink $PWD/spotify/spotify-flags.conf --to-config
 fi
--- symlink.sh
+++ symlink.sh
@@ -4,48 +4,48 @@ function symlink {
 	}
 
 	if [ $# -lt 1 ]; then
-	  help
-	  return 1
+		help
+		return 1
 	fi
 
 	source="$1"
 	shift
 	PARSED=$(getopt -o '' --long to-home,to-config,custom-dir: -- "$@")
 	if [[ $? -ne 0 ]]; then
-	  return 1
+		return 1
 	fi
 	eval set -- "$PARSED"
 
 	target_dir=""
 	while true; do
-	  case "$1" in
+		case "$1" in
 		--to-home)
-		  target_dir="$HOME"
-		  shift
-		  ;;
+			target_dir="$HOME"
+			shift
+			;;
 		--to-config)
-		  target_dir="$HOME/.config"
-		  shift
-		  ;;
+			target_dir="$HOME/.config"
+			shift
+			;;
 		--custom-dir)
-		  target_dir="$2"
-		  shift 2
-		  ;;
+			target_dir="$2"
+			shift 2
+			;;
 		--)
-		  shift # End of options
-		  break
-		  ;;
+			shift # End of options
+			break
+			;;
 		*)
-		  echo "Invalid option: $1" >&2
-		  return 1
-		  ;;
-	  esac
+			echo "Invalid option: $1" >&2
+			return 1
+			;;
+		esac
 	done
 
 	# If no valid options were provided (target_dir is empty), show an error and exit
 	if [ -z "$target_dir" ]; then
-	  help
-	  return 1
+		help
+		return 1
 	fi
 
 	target="$target_dir/$(basename $source)"
